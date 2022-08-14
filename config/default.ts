2f9fb74f08cfaee5ec042be2740f4a39b3ff8506
Jeffrey <1115378579@qq.com>
2022-08-14 17:56:57 +0800

    Create default.ts

---
+++ config/default.ts
@@ -0,0 +1,17 @@
+export default {
+  port: 3100,
+  dburl: '',
+  mysqlConfig: {
+    database: 'my-website',
+    username: 'admin',
+    password: 'mc1009jf1018.',
+    host: 'bj-cynosdbmysql-grp-0o0dqcfy.sql.tencentcdb.com',
+    port: 21729,
+  },
+  mysqlConfig2: {
+    database: 'ikun',
+    username: '25dov4edthu7',
+    password: 'pscale_pw_19dmPNnvIIo2jqL4Nx4sWO6rbsJHeC56KaVj_100dts.',
+    host: 'vw2be4781rkz.us - east - 2.psdb.cloud',
+  },
+}
